54ad2b830951be4d5af8691d1c934d40dd481d1d
akashmehta <akash.mehta@lucidworks.com>
2016-06-22 15:22:24 -0700

    Adding some more examples

--- labs/eventsim/TestScript.scala
+++ labs/eventsim/TestScript.scala
@@ -1,2 +1,8 @@
-var Allevents=sqlContext.read.format("solr").options(Map("zkHost" -> "localhost:9983", "collection" -> "eventsim","time_series_partition_on" -> "true" ,"time_period" -> "1DAYS","time_stamp_field_name" -> "ts","solr.params" -> "fq=ts:[* TO *]")).load
-Allevents.count()
+var allEvents=sqlContext.read.format("solr").options(Map("zkHost" -> "localhost:9983", "collection" -> "eventsim","partition_by" -> "time" ,"time_period" -> "1DAYS","time_stamp_field_name" -> "ts","solr.params" -> "fq=ts:[* TO *]")).load
+var someEvents=sqlContext.read.format("solr").options(Map("zkHost" -> "localhost:9983", "collection" -> "eventsim","partition_by" -> "time" ,"time_period" -> "1DAYS","time_stamp_field_name" -> "ts","solr.params" -> "fq=ts:[2016-06-21T00:00:00Z TO 2016-06-23T00:00:00Z]")).load
+var tillEnd=sqlContext.read.format("solr").options(Map("zkHost" -> "localhost:9983", "collection" -> "eventsim","partition_by" -> "time" ,"time_period" -> "1DAYS","time_stamp_field_name" -> "ts","solr.params" -> "fq=ts:[2016-06-21T00:00:00Z TO *]")).load
+var eventsWithShardSplit=sqlContext.read.format("solr").options(Map("zkHost" -> "localhost:9983", "collection" -> "eventsim","partition_by" -> "time" ,"time_period" -> "1DAYS","time_stamp_field_name" -> "ts","solr.params" -> "fq=ts:[* TO *]","split_field" -> "_version_","splits_per_shard" -> "4")).load
+someEvents.count
+allEvents.count
+tillEnd.count
+eventsWithShardSplit.count
